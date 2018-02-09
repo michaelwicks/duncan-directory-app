c85c12e1b113d30506de76d900098f28a5b4f7e3
mshan10 <36176694+mshan10@users.noreply.github.com>
2018-02-09 16:58:39 -0500

    Floor image (#1)
    * added project
    
    * added floor image feature
    
    * replaced spaces with underscores
    
    * added floor image links
    
    *  changed links in floorimage
    
    * changed links to linkaas
    
    * changed spacing in floor_image.json

---
+++ back-end/app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+var search = require('./search');
+var image = require('./image');
+
+app.use('/', search);
+app.use('/', image);
+
+app.listen(port);
