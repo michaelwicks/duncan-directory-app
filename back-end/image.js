4012d9c5923562a33b25a837b22e5585e4b03fa6
Jewon Oh <joh0531@gmail.com>
2018-02-09 17:38:00 -0500

    updated floor image feature

--- back-end/image.js
+++ back-end/image.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+function search(floorNum) {
+    var floorImage = require('./floor_image.json');
+    for (var i = 0; i < floorImage.length; i++) {
+        if (floorNum == floorImage[i].floor)
+            return floorImage[i].link;
+        if (i == floorImage.length - 1) {
+            return "floor does not exist";
+        }
+    }
+}
+
 router.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -8,8 +19,8 @@ router.use(function(req, res, next) {
 });
 
 router.get('/image', function(req, res) {
-    var images = require('./floor_image.json');
-    res.send(images);
+    var floorNum = req.query.floor_num;
+    res.send(search(floorNum));
 });
 
 module.exports = router;
