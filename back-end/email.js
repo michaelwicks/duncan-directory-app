9484d3547f32d8228e147f19d9632ba23dccc3d7
mshan10 <mshan@nd.edu>
2018-02-22 16:44:30 -0500

    added testing for parse on buddy

--- back-end/email.js
+++ back-end/email.js
@@ -1,3 +1,8 @@
 var sparkpost = require('sparkpost');
 //write email class...
+var email = {};
+email.send = send;
+function send(emailAddress, name, keyword){
+  console.log("email sent")
+}
 module.exports = email;
