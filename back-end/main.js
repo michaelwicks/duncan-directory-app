71e2b46d2f4a16d172f31419bb20896de5f9faf4
awalton3 <awalton3@nd.edu>
2018-03-26 20:10:57 -0400

    wip: compares keyword in event aand email Parse classes and returns array of email recipient matches

--- back-end/main.js
+++ back-end/main.js
@@ -24,3 +24,70 @@ Parse.Cloud.afterSave("Email", function(request) {
     console.error('Request failed ' + httpResponse.status);
   });
 });
+
+
+Parse.Cloud.afterSave("Event", function(request) {
+    
+    console.log(request.object); 
+    request.log.info(request.object);
+    
+    // defining event variables  
+    var eventKeyword = request.object.get("keyword"); 
+    var title = request.object.get("title");
+    var description = request.object.get("description"); 
+    var location = request.object.get("location"); 
+    
+    // Query Email class
+    
+    
+    
+    // Check if keywords are equal 
+    
+    var emailList = [];
+    
+    // Return array of matches (email parse objects)
+    
+    var matchEmail = emailList.map(function(emailObject) {
+        
+        // Get keywordString
+        
+        var emailKeyword = emailObject.get("keywordString"); 
+        
+        // Alert front-end team to filter lowercase on event form save 
+
+        
+        if (emailKeyword === eventKeyword) {
+            
+                return emailObject;
+            
+            }
+        
+        
+    }); 
+    
+            // Create array of recipient email
+    
+    var recipientList = matchEmail.map(function(matchedObject, emailKeyword) {
+                                       
+             //Get email address 
+                                       
+        var matchedEmail = matchedObject.get("email"); 
+        var matchedName = matchedObject.get("name"); 
+        
+           return {     
+      "address": {
+        "email": matchedEmail,
+        "name": matchedName
+      },
+      "tags": [],
+        "metadata": {},
+      "substitution_data": {
+        "name": matchedName,
+        "keyword": emailKeyword
+      }
+    }
+                                       
+    });  
+
+}); 
+
